a82d79fae3bbb4dda15ead5cdf2175f328006f70
sushiguruma <sushiguruma.honeycomb321@gmail.com>
2018-11-19 12:47:37 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
   def index
     @users = User.all.page(params[:page])
@@ -51,7 +51,18 @@ class UsersController < ApplicationController
     flash[:success] = 'ユーザは正常に削除されました。'
     redirect_to '/'
   end
-    
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
   
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   get 'microposts/create'
 
   get 'microposts/destroy'
@@ -11,7 +15,13 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
